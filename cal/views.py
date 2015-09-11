8bcfef3e33ed6d162c55158b96af1826f616edeb
Max Fang <maxfangx@gmail.com>
2015-09-10 23:56:45 -0700

    Split logged in and logged out homepages

--- cal/views.py
+++ cal/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render_to_response
 
 
 def home(request):
-    return render_to_response(template_name='home.html')
+    if request.user.is_authenticated():
+        return render_to_response(template_name='home_logged_in.html')
+    return render_to_response(template_name='home_logged_out.html')
