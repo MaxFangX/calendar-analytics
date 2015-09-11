e4f1fc972eb9156a973fdbe6afc945be3cd27007
Max Fang <maxfangx@gmail.com>
2015-09-11 00:09:05 -0700

    Log out

--- cal/urls.py
+++ cal/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'cal.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout$', 'cal.views.logout', name='logout'),
 
 )
--- cal/views.py
+++ cal/views.py
@@ -6,3 +6,8 @@ def home(request):
     if request.user.is_authenticated():
         return render_to_response(template_name='home_logged_in.html')
     return render_to_response(template_name='home_logged_out.html')
+
+
+def logout(request):
+    logout(request)
+    return render_to_response(template_name='home_logged_out.html')
