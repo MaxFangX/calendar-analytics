b445bdeb754868b70dba29192040ef515c973f2e
Max Fang <maxfangx@gmail.com>
2015-10-20 02:31:14 -0700

    Use ModelSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,21 +3,21 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
-class GEventSerializer(serializers.HyperlinkedModelSerializer):
+class GEventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = GEvent
         fields = ('name', 'start_time', 'end_time', 'color', 'note')
 
 
-class StatisticSerializer:
+class StatisticSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Statistic
