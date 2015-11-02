f1f74bf0540ac56ffda993483a640a33f0626e29
Max Fang <maxfangx@gmail.com>
2015-11-02 04:33:04 -0800

    Static files config

--- cal/settings.py
+++ cal/settings.py
@@ -104,8 +104,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+if ENVIRONMENT == 'prod':
+    STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+    STATIC_URL = '/static/'
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+    STATIC_URL = '/static/'
 
 # These environment variables are set in prod
 if 'RDS_DB_NAME' in os.environ:
