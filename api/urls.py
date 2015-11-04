ece46c59383ec147357a8eddcb6c7ebe49b79162
Max Fang <maxfangx@gmail.com>
2015-11-04 05:26:20 -0800

    Router.urls dammit I\'m busy

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,16 @@
 from api import views
 from django.conf.urls import include, patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 urlpatterns = patterns(
 
     url(r'^v1/$', views.api_root),
     url(r'^v1/gevents/$', views.GEventList.as_view()),
     url(r'^v1/stats/$', views.StatisticList.as_view()),
+    url(r'^v1/', include(router.urls)),
 
 )
 
