e60b7d6079df5459b08f11411482c697ff40b031
Max Fang <maxfangx@gmail.com>
2015-11-05 21:41:44 -0800

    Use mysqlite temporarily on dev

--- cal/settings.py
+++ cal/settings.py
@@ -80,12 +80,34 @@ WSGI_APPLICATION = 'cal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    # These environment variables are set in prod
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    # DATABASES['default'] = {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.getenv('CJ_DB_NAME'),
+    #     'USER': os.getenv('CJ_DB_USER'),
+    #     'PASSWORD': os.getenv('CJ_DB_PASSWORD'),
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    # }
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -110,28 +132,6 @@ if ENVIRONMENT == 'prod':
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
     STATIC_URL = '/static/'
-
-# These environment variables are set in prod
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('CJ_DB_NAME'),
-        'USER': os.getenv('CJ_DB_USER'),
-        'PASSWORD': os.getenv('CJ_DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
-    }
 
 # Production settings
 if ENVIRONMENT == 'prod':
