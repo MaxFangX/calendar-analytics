ce953626f4084723910d6cada3f19268ddb35fe1
Max Fang <maxfangx@gmail.com>
2015-11-08 23:34:58 -0800

    Base tests

---
+++ cal/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from cal.models import GCalendar, GEvent
+
+class FindCategoryTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_separation_of_categories(self):
+        pass
