e3cffbbc5ec9cbbc20371e144658fe5bd771d79a
Max Fang <maxfangx@gmail.com>
2015-11-09 23:21:02 -0800

    More test skeleton i'm too busy ugh

--- cal/tests.py
+++ cal/tests.py
@@ -1,10 +1,14 @@
+from django.auth.models import User
 from django.test import TestCase
 from cal.models import GCalendar, GEvent
 
 class FindCategoryTestCase(TestCase):
 
     def setUp(self):
-        pass
+        self.alice = User(username='alice')
+        self.alice.save()
+        self.calendar = GCalendar(user=self.alice)
+        self.calendar.save()
 
     def test_separation_of_categories(self):
         pass
