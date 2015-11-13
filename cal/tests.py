ce4eeeb5edc02256cc51c806f38114a96d6e182d
Max Fang <maxfangx@gmail.com>
2015-11-12 22:56:38 -0800

    Test categories

--- cal/tests.py
+++ cal/tests.py
@@ -1,8 +1,10 @@
 from django.auth.models import User
 from django.test import TestCase
-from cal.models import GCalendar, GEvent
+from cal.models import GCalendar, GEvent, Statistic, EVENT_COLORS
 
-class FindCategoryTestCase(TestCase):
+import datetime
+
+class CategoryTestCase(TestCase):
 
     def setUp(self):
         self.alice = User(username='alice')
@@ -11,4 +13,25 @@ class FindCategoryTestCase(TestCase):
         self.calendar.save()
 
     def test_separation_of_categories(self):
-        pass
+
+        now = datetime.datetime.now()
+        yesterday = now - datetime.timedelta(days=1)
+
+        e1 = GEvent(name='bigger',
+                    start_time=yesterday,
+                    end_time=yesterday+datetime.timedelta(hours=1),
+                    calendar=self.calendar, color=EVENT_COLORS[0])
+        e1.save()
+        e2 = GEvent(name='bigger smaller',
+                    start_time=yesterday,
+                    end_time=yesterday+datetime.timedelta(hours=1),
+                    calendar=self.calendar, color=EVENT_COLORS[0],)
+        e2.save()
+
+        stat1 = Statistic(user=self.alice,
+                          name='Bigger',
+                          start_time=yesterday,
+                          end_time=now,)
+        stat1.save()
+
+        self.assertEqual(2, stat1.query().count())
