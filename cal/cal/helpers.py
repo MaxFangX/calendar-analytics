b2545e8419414ce4fbedf5111bdc9557982e7376
Max Fang <maxfangx@gmail.com>
2016-02-05 23:59:50 -0800

    Beginnings of TimeNode

--- cal/cal/helpers.py
+++ cal/cal/helpers.py
@@ -6,3 +6,12 @@ import json
 
 def json_response(data, status=200):
     return HttpResponse(json.dumps(data, cls=DjangoJSONEncoder), status=status, content_type="application/json")
+
+
+class TimeNode:
+
+    def __init__(self, start, end, event_id=None):
+        self.event_id = event_id
+        self.tail = None
+        self.start = start
+        self.end = end
