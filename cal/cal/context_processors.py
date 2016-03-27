c3240c71ea462188eda0d7d273c97b22d66a654e
Max Fang <maxfangx@gmail.com>
2016-03-26 19:09:50 -0700

    Remove cruft

--- cal/cal/context_processors.py
+++ cal/cal/context_processors.py
@@ -6,5 +6,5 @@ def calendar_analytics_processor(request):
     return {
             'social_auth_google_plus_key': settings.SOCIAL_AUTH_GOOGLE_PLUS_KEY,
             # Space separated string of scopes
-            'social_auth_google_plus_scope': " ".join(map(str, settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE)),
+            'social_auth_google_oauth2_scope': " ".join(map(str, settings.SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE)),
             }
