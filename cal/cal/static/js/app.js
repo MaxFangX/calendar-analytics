37c1b018351e482a5f250ee71414d7129bf2b2af
Max Fang <maxfangx@gmail.com>
2016-08-20 18:03:07 -0700

    Show/hide calendars

--- cal/cal/static/js/app.js
+++ cal/cal/static/js/app.js
@@ -6,64 +6,75 @@ analyticsApp.controller('LoggedInCtrl', function LoggedInController($scope) {
 
 });
 
-analyticsApp.controller('CalendarCtrl', function UiCalendarCtrl($scope, $http, $q) {
+analyticsApp.controller('CalendarCtrl', function UiCalendarCtrl($scope, $http, $q, uiCalendarConfig) {
 
-    $scope.events = function(start, end, timezone, callback) {
-      var query_timezone = '';
-      if (!timezone) {
-        query_timezone = 'UTC';
-      } else if (timezone === 'local') {
-        query_timezone = moment.tz.guess();
-      } else {
-        query_timezone = timezone;
-      }
-      allEvents = [];
+    $scope.calendars = {}
 
-      $http({
-        method: 'GET',
-        url: "/v1/gcalendars.json",
-        cache: true,
-      }).then(function gcalSuccess(response) {
+    this.events = function(start, end, timezone, callback) {
+        var query_timezone = '';
+        if (!timezone) {
+            query_timezone = 'UTC';
+        } else if (timezone === 'local') {
+            query_timezone = moment.tz.guess();
+        } else {
+            query_timezone = timezone;
+        }
+        allEvents = [];
 
-        var collectedEvents = [];
-        var eventPromises = response.data.results.map(function(gcal) {
-          return $http({
+        $http({
             method: 'GET',
-            url: "/v1/gevents.json",
+            url: "/v1/gcalendars.json",
             cache: true,
-            params: {
-              calendarId: gcal.calendar_id,
-              start: start.toISOString(),
-              end: end.toISOString(),
-              timezone: query_timezone,
-              edge: 'truncated'
-            },
-          }).then(function eventSuccess(response) {
-              var events = [];
-              $.each(response.data.results, function(index, gevent) {
-                events.push({
-                  title: gevent.name,
-                  start: gevent.start,
-                  end: gevent.end,
-                  backgroundColor: gevent.color.background,
-                  textColor: gevent.color.foreground,
-                  borderColor: gevent.color.foreground,
-                })
+        }).then(function gcalSuccess(response) {
+
+            var collectedEvents = [];
+            var eventPromises = response.data.results.map(function(gcal) {
+
+              // Initialize cached calendars
+              if ($scope.calendars[gcal.calendar_id] === undefined) {
+                  $scope.calendars[gcal.calendar_id] = gcal;
+                  $scope.calendars[gcal.calendar_id].enabled = true;
+              }
+
+              return $http({
+                  method: 'GET',
+                  url: "/v1/gevents.json",
+                  cache: true,
+                  params: {
+                      calendarId: gcal.calendar_id,
+                      start: start.toISOString(),
+                      end: end.toISOString(),
+                      timezone: query_timezone,
+                      edge: 'truncated'
+                  },
+              }).then(function eventSuccess(response) {
+                  var events = [];
+                  $.each(response.data.results, function(index, gevent) {
+                      events.push({
+                          title: gevent.name,
+                          start: gevent.start,
+                          end: gevent.end,
+                          backgroundColor: gevent.color.background,
+                          textColor: gevent.color.foreground,
+                          borderColor: gevent.color.foreground,
+                      })
+                  });
+
+                  if ($scope.calendars[gcal.calendar_id].enabled) {
+                      collectedEvents = collectedEvents.concat(events);
+                  }
+              }, function eventError(response) {
+                  console.log("Ajax call to gevents failed: " + response);
               });
-              // TODO if check here for if the calendar is enabled
-              collectedEvents = collectedEvents.concat(events);
-          }, function eventError(response) {
-            console.log("Ajax call to gevents failed: " + response);
-          });
-        });
+            });
 
-        $q.all(eventPromises).then(function() {
-          callback(collectedEvents);
-        });
+            $q.all(eventPromises).then(function() {
+                callback(collectedEvents);
+            });
 
-      }, function gcalError(response) {
-        console.log("Ajax call to gcalendars failed: " + response);
-      })
+        }, function gcalError(response) {
+            console.log("Ajax call to gcalendars failed: " + response);
+        })
 
     };
 
@@ -80,5 +91,11 @@ analyticsApp.controller('CalendarCtrl', function UiCalendarCtrl($scope, $http, $
       }
     };
 
-    $scope.eventSources = [$scope.events];
+    this.refresh = function(calendarName) {
+      if(uiCalendarConfig.calendars[calendarName] !== undefined){
+        uiCalendarConfig.calendars[calendarName].fullCalendar('refetchEvents');
+      }
+    };
+
+    this.eventSources = [this.events];
 })
