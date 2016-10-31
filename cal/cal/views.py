06224c28fef0e68f30e5810bbe9676fcc70c02fe
Max Fang <maxfangx@gmail.com>
2016-10-30 23:09:35 -0700

    Change Google OAuth include_granted_scopes parameter to string

--- cal/cal/views.py
+++ cal/cal/views.py
@@ -77,7 +77,7 @@ def google_auth(request):
                                        scope=['https://www.googleapis.com/auth/calendar','profile','email'],
                                        redirect_uri=settings.BASE_URL + '/auth/google')
     default_flow.params['access_type'] = 'offline'
-    default_flow.params['include_granted_scopes'] = True
+    default_flow.params['include_granted_scopes'] = 'true'
 
     # Try to retrieve an existing flow, or create one if it doesn't exist
     gflow = GoogleFlow.objects.filter(id=request.user).last()
