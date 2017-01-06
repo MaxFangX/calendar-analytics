ac859648a5e66d78c961d7d00db7c91d4c50926d
James Uejio <jamesuejio@berkeley.edu>
2017-01-06 12:19:31 -0800

    Insert helper function into Query Service

--- cal/cal/static/js/services.js
+++ cal/cal/static/js/services.js
@@ -262,27 +262,27 @@ analyticsApp.service('QueryService', function() {
 
     return [ctrlDetails, maxYValue];
   };
+
+  // returns slope, intercept and r-square of the line
+  function leastSquares(xSeries, ySeries) {
+    var reduceSumFunc = function(prev, cur) { return prev + cur; };
+
+    var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+    var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+    var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+    .reduce(reduceSumFunc);
+
+    var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+    .reduce(reduceSumFunc);
+
+    var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+    .reduce(reduceSumFunc);
+
+    var slope = ssXY / ssXX;
+    var intercept = yBar - (xBar * slope);
+    var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+    return [slope, intercept, rSquare];
+  }
 });
-
-// returns slope, intercept and r-square of the line
-function leastSquares(xSeries, ySeries) {
-  var reduceSumFunc = function(prev, cur) { return prev + cur; };
-
-  var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
-  var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
-
-  var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
-  .reduce(reduceSumFunc);
-
-  var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
-  .reduce(reduceSumFunc);
-
-  var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
-  .reduce(reduceSumFunc);
-
-  var slope = ssXY / ssXX;
-  var intercept = yBar - (xBar * slope);
-  var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
-
-  return [slope, intercept, rSquare];
-}
