0a17d3100c9917bb39cfc08078cb20970e763cd7
Cindy Tung <cindytung@berkeley.edu>
2017-01-06 16:24:14 -0700

    fixed spaces bug

--- cal/cal/models.py
+++ cal/cal/models.py
@@ -645,7 +645,7 @@ class Tag(models.Model, EventCollection):
 
         querysets = [
                 GEvent.objects
-                .filter(calendar=calendar, name__regex=r'(?i)[^a-zA-Z\d\s:]?'+keyword+r'(?i)[^a-zA-Z\d\s:]')
+                .filter(calendar=calendar, name__regex=r'(?i)[^a-zA-Z\d:]?'+keyword+r'(?i)[^a-zA-Z\d:]')
                 .exclude(all_day_event=True)
                 for keyword in keywords
                 for calendar in calendars
