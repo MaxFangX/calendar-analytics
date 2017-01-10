1b9094bb357f2215337798323d1f7dcf634e30d0
James Uejio <jamesuejio@berkeley.edu>
2017-01-10 15:46:02 -0800

    Handle edge case Tag with no hours

--- cal/cal/helpers.py
+++ cal/cal/helpers.py
@@ -75,7 +75,10 @@ def get_time_series(model, timezone='UTC', time_step='week', calendar_ids=None, 
     week_hours = []
     events = model.query(calendar_ids, start, end).order_by('start')
     i = 0
-    # Convert start to local time
+    # Takes care of Tags with no hours
+    if not events:
+        return [(0,0)]
+    # Convert start to local times
     start = events[0].start.astimezone(pytz.timezone(timezone))
     if time_step == 'day':
         # To indicate do nothing if Daily is passed in
