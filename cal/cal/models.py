0299f7b14c2c24df848ca0c16524a1ecde1bd4b8
Cindy Tung <cindytung@berkeley.edu>
2017-01-22 14:30:24 -0800

    merge master

--- cal/cal/models.py
+++ cal/cal/models.py
@@ -59,7 +59,7 @@ class Profile(models.Model):
         if calendar_ids:
             for calendar_str in calendar_ids:
                 try:
-                    c = GCalendar.objects.filter(calendar_id=calendar_str).first()
+                    c = GCalendar.objects.get(calendar_id=calendar_str)
                 except GCalendar.DoesNotExist:
                     raise InvalidParameterException("Provided calendar {} does not exist".format(calendar_str))
                 if c.user != self.user:
