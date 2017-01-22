14b0bb9ae6006f3ce5ab7260cac8d5b674d074ae
Andrew Huang <andrew.huang.010@gmail.com>
2017-01-22 10:21:37 -0800

    [wip] single page app

--- cal/cal/static/js/app.js
+++ cal/cal/static/js/app.js
@@ -1,6 +1,41 @@
 /*jslint devel: true, browser: true, jquery: true */
 
-var analyticsApp = window.angular.module('analyticsApp', ['analyticsApp.services', 'nvd3', 'ui.calendar']);
+var analyticsApp = window.angular.module('analyticsApp', [
+  'analyticsApp.services',
+  'nvd3',
+  'ui.calendar',
+  'ui.router'
+]);
 
-analyticsApp.controller('LoggedInCtrl', function LoggedInController() {
+
+analyticsApp.config(['$stateProvider', function ($stateProvider) {
+
+  $stateProvider.state('home', {
+    url: '',
+    component: 'calendar'
+  });
+
+  $stateProvider.state('tag-details', {
+    url: '/tag/{tagId}',
+    resolve: {
+      tagId: function($stateParams) { return $stateParams.tagId; }
+    },
+    component: 'tagDetails'
+  });
+
+  $stateProvider.state('category-details', {
+    url: '/category/{categoryId}',
+    resolve: {
+      categoryId: function ($stateParams) { return $stateParams.categoryId; },
+      categoryHours: function ($stateParams) { return $stateParams.categoryHours; }
+    },
+    component: 'categoryDetails'
+  });
+
+}]);
+
+// Debugging
+analyticsApp.run(function($rootScope) {
+    $rootScope.$on("$stateChangeError", console.log.bind(console));
 });
+
