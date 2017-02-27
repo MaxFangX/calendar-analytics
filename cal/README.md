037b90f7c2b2fd8cb101a558bb41e1ddfd0d1984
Andrew Huang <andrewhuang010@gmail.com>
2017-02-26 21:32:21 -0800

    cleanup

--- cal/README.md
+++
@@ -1,8 +0,0 @@
-sass --watch .
-
-python manage.py runserver
-
-~/ngrok http 8000 -subdomain=maxfangx
-
-# https://github.com/nvbn/django-bower
-python manage.py bower install
--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -194,12 +194,12 @@ USE_TZ = True
 if ENVIRONMENT == 'prod':
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    
+
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    
+
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATIC_URL = '/static/'
-    
+
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
