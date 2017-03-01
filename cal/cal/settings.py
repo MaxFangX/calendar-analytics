d36d12f74b39e48877b33f4de97de2d22b7f5e7d
Andrew Huang <andrewhuang010@gmail.com>
2017-03-01 10:58:37 -0800

    tweak?

--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -202,7 +202,7 @@ if ENVIRONMENT == 'prod':
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(PROJECT_ROOT, '../static'),
     )
 
     BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "..", "www", "cal", "static/")
