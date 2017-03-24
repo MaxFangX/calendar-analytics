ddfedd22d1bd0d042bfa7e168a468eba440ef04b
James Uejio <jamesuejio@berkeley.edu>
2017-03-24 03:31:58 -0700

    REmove

--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -199,9 +199,9 @@ if ENVIRONMENT == 'prod':
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
     STATIC_ROOT = os.path.join(BASE_DIR, "cal", "static/")
     STATIC_URL = 'cal/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "cal", "static/"),
-    )
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, "cal", "static/"),
+    # )
     BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "cal", "static/")
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, "static/")
