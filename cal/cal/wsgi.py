65529c445dbfc2567ed08ca98c70e7255b817ff0
James Uejio <jamesuejio@berkeley.edu>
2017-03-24 01:55:23 -0700

    add

--- cal/cal/wsgi.py
+++ cal/cal/wsgi.py
@@ -11,4 +11,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cal.settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
