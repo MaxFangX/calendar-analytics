fa1b122a8bdfd0cc973fd169e41c0449e5c17d8c
James Uejio <jamesuejio@berkeley.edu>
2017-03-25 15:21:23 -0700

    Fixed settings

--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -207,10 +207,11 @@ else:
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATIC_URL = '/static/'
+    print PROJECT_ROOT
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
-    BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "..", "www", "cal", "static/")
+    BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "..", "static/")
 
 STATICFILES_FINDERS = [
         'django.contrib.staticfiles.finders.FileSystemFinder',
