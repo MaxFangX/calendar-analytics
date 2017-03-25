4234bbc2cd620c18d2d8374a586e663032e0c269
James Uejio <jamesuejio@berkeley.edu>
2017-03-25 14:18:59 -0700

    Tried to fix staticfiles.

--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -207,7 +207,7 @@ else:
     # STATICFILES_DIRS = (
     #     os.path.join(PROJECT_ROOT, '../static'),
     # )
-    BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "cal", "static/")
+    BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_FINDERS = [
         'django.contrib.staticfiles.finders.FileSystemFinder',
