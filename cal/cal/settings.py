3d350da20c66066ec9b51290adaf345615d80e8a
James Uejio <jamesuejio@berkeley.edu>
2017-03-25 15:12:24 -0700

    Add nostatic.

--- cal/cal/settings.py
+++ cal/cal/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     # CalendarJournal App(s)
