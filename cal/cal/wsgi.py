626e08908c69fa4e8c7bdca1d3e0e00fcd32a81e
James Uejio <jamesuejio@berkeley.edu>
2017-03-25 13:54:46 -0700

    Reorder files

--- cal/cal/wsgi.py
+++ cal/cal/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cal.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
